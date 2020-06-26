3c7372ed386e694733271d83397581c73ebb1df8
s19273 <s19273@pjwstk.edu.pl>
2020-06-26 14:10:08 +0200

    Cwiczenia 12

---
+++ APBD_Cwiczenie12/Data/MvcMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using APBD_Cwiczenie12.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD_Cwiczenie12.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
